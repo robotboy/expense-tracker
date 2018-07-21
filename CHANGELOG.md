bc16561298a416d1dcd6c865b51f9e0c0734f655
Frenky Harry <frenky.harry@go-pay.co.id>
2018-07-21 23:00:48 +0700

    Add Bootstrap 4.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+1.0.1
+----
+ * Added Bootstrap for Rails 4.1.1 
+
 1.0.0
 ----
  * Initialize project using command: `rails new expense-tracker --database=postgresql`
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'tracker/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'tracker#index'
 end
