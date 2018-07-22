d75b3234a978a03625d8180086c1ec47e53a3b7c
Frenky Harry <frenky.harry@go-pay.co.id>
2018-07-22 14:46:57 +0700

    Login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
+
   get 'tracker/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
